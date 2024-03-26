e553cead014d444bdbabe0e7dfcba74a4ac5b732
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-03-25 21:28:08 -0300

    Add files via upload

---
+++ solSoma/projSoma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projSoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            string aux;
+
+            Console.Write("Digite um numero: ");
+            aux = Console.ReadLine();
+            a = int.Parse(aux);
+
+            Console.Write("Digite outro numero: ");
+            aux = Console.ReadLine();
+            b = int.Parse(aux);
+
+            c = a + b;
+
+            Console.Write("A soma dos números é: ");
+            Console.WriteLine(c);
+            Console.WriteLine();
+
+        }
+    }
+}
